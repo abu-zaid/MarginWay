d598f87a0d2c513409b77eb585022eb0e111c6f2
Abu Zaid <iam.abuzaid@gmail.com>
2024-01-17 22:11:42 +0530

    Update maxDuration

--- app/api/cron/route.ts
+++ app/api/cron/route.ts
@@ -10,7 +10,7 @@ import {
   getLowestPrice,
 } from "@/lib/util";
 import { NextResponse } from "next/server";
-export const maxDuration = 300;
+export const maxDuration = 10;
 export const dynamic = 'force-dynamic';
 export const revalidate =0;
 export async function GET() {
