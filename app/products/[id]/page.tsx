dc3b34c872999871f6e514dbfb79512bbb3cf41d
Abu Zaid <iam.abuzaid@gmail.com>
2024-01-18 23:26:38 +0530

    Dont show modal on page open

--- app/products/[id]/page.tsx
+++ app/products/[id]/page.tsx
@@ -161,7 +161,7 @@ const ProductDetails = async ({ params: { id } }: Props) => {
             </div>
           </div>
 
-          <Modal productId={id} />
+          {/* <Modal productId={id} /> */}
         </div>
       </div>
 
