c043aaf2798b970d8894c58a51668d69181b4727
Abu Zaid <iam.abuzaid@gmail.com>
2024-01-20 15:49:52 +0530

    Add log

--- lib/nodemailer/index.ts
+++ lib/nodemailer/index.ts
@@ -87,6 +87,7 @@ export const sendEmail = async (emailContent: EmailContent, emails: string[]) =>
         html: emailContent.body,
         subject: emailContent.subject,
     }
+    console.log("Inside send email function");
     transporter.sendMail(mailOptions, (error: any, info: any) => {
         if(error) return console.log(error);
         
