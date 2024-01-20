15c5aa1e2c41a3efa5a6770486057fe0ab35bbd9
Abu Zaid <iam.abuzaid@gmail.com>
2024-01-20 23:46:42 +0530

    Mail changes

--- app/defer/mailer.ts
+++ app/defer/mailer.ts
@@ -2,29 +2,29 @@ import { EmailContent } from "@/types";
 import nodemailer from "nodemailer";
 import { defer } from "@defer/client";
 
-var transporter = nodemailer.createTransport ({
-    host: 'smtppro.zoho.in',
+async function sendEmail(emailContent: EmailContent, emails: string[]) {
+  console.log("Inside sendEmail");
+  console.log("Emails list:", emails);
+  const mailOptions = {
+    from: String(process.env.EMAIL_ADDRESS),
+    bcc: emails,
+    html: emailContent.body,
+    subject: emailContent.subject,
+  };
+  var transporter = nodemailer.createTransport({
+    host: "smtppro.zoho.in",
     port: 465,
     secure: true, // use SSL
-    auth:{
+    auth: {
       user: String(process.env.EMAIL_ADDRESS),
-      pass: String(process.env.EMAIL_PASSWORD)
-  },
+      pass: String(process.env.EMAIL_PASSWORD),
+    },
   });
-  async function sendEmail(emailContent: EmailContent, emails: string[]) {
-    console.log('Inside sendEmail');
-    console.log('Emails list:',emails);
-      const mailOptions = {
-          from: String(process.env.EMAIL_ADDRESS),
-          bcc: emails,
-          html: emailContent.body,
-          subject: emailContent.subject,
-      }
-      transporter.sendMail(mailOptions, (error: any, info: any) => {
-          if(error) return console.log(error);
-          
-          console.log('Email sent: ', info);
-        })
-  }
+  transporter.sendMail(mailOptions, (error: any, info: any) => {
+    if (error) return console.log(error);
 
-  export default defer(sendEmail);
+    console.log("Email sent: ", info);
+  });
+}
+
+export default defer(sendEmail);
