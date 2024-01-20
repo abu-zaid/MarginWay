d4ceadbab07a308a8d7d5d7594cf7949130d598f
Abu Zaid <iam.abuzaid@gmail.com>
2024-01-20 13:43:44 +0530

    add logs

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -4,9 +4,9 @@ let isConnected = false;
 
 export const connectToDatabase = async () => {
   mongoose.set("strictQuery", true);
-
+  console.log("mong uri",process.env.MONGO_DB_URI)
   if (!process.env.MONGO_DB_URI) return console.log("MongoDB URI is required!");
-  if (isConnected) return console.log("Using Existing database connection!");
+  if (isConnected) return isConnected;
 
   try {
     await mongoose.connect(process.env.MONGO_DB_URI);
