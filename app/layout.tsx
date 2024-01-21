cf13e16e87672b0356c31c3e2ccf5152ef462168
Abu Zaid <iam.abuzaid@gmail.com>
2024-01-21 12:54:05 +0530

    Add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/dist/next";
 
 const inter = Inter({ subsets: ["latin"] });
 const space_grotest = Space_Grotesk({
@@ -22,12 +23,18 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <head><script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2563630042127627"></script></head>
+      <head>
+        <script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2563630042127627"
+        ></script>
+      </head>
       <body className={inter.className}>
         <main className="max-w-10xl mx-auto">
           <Navbar />
           {children}
           <Analytics />
+          <SpeedInsights />
         </main>
       </body>
     </html>
