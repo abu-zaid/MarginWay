3e7ea30070651c25225913a31677024b31239647
Abu Zaid <iam.abuzaid@gmail.com>
2024-01-21 12:56:13 +0530

    Fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,7 +3,7 @@ import { Inter, Space_Grotesk } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import { Analytics } from "@vercel/analytics/react";
-import { SpeedInsights } from "@vercel/speed-insights/dist/next";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 const inter = Inter({ subsets: ["latin"] });
 const space_grotest = Space_Grotesk({
