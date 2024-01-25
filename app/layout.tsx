26e900877cf80f449f981ebaffad180f1a22b2c9
Abu Zaid <iam.abuzaid@gmail.com>
2024-01-26 02:24:33 +0530

    style: Use Poppins in navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
         <script
           async
           src="https://www.googletagmanager.com/gtag/js?id=G-2JJMRDRHML"
