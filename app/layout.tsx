92286288c61deaa982f744e7a90220a61d328b85
Abu Zaid <iam.abuzaid@gmail.com>
2024-01-25 16:21:06 +0530

    build: update GA

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,6 @@ import Navbar from "@/components/Navbar";
 import { Analytics } from "@vercel/analytics/react";
 import { SpeedInsights } from "@vercel/speed-insights/next";
 import GoogleAnalytics from "@/components/GoogleAnalytics";
-
 const inter = Inter({ subsets: ["latin"] });
 const space_grotest = Space_Grotesk({
   subsets: ["latin"],
@@ -30,10 +29,32 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-2JJMRDRHML"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-2JJMRDRHML');/>
+          }} `,
+          }}
+        />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2563630042127627"
+        ></script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-XXXXXXXXXX"
+        ></script>
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-2JJMRDRHML"
         ></script>
       </head>
       <body className={poppins.className}>
