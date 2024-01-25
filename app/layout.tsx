ce266041e7ec4c3f9772be9d696ebb928965170b
Abu Zaid <iam.abuzaid@gmail.com>
2024-01-25 13:42:13 +0530

    build: apple web app capable?

--- app/layout.tsx
+++ app/layout.tsx
@@ -29,6 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        <meta name="apple-mobile-web-app-capable" content="yes" />
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2563630042127627"
