97291f4655bffc72b6b176c3052b7cef1df1669a
Abu Zaid <iam.abuzaid@gmail.com>
2024-01-27 16:49:34 +0530

    build: update chromium verssion

--- lib/puppeteer/index.ts
+++ lib/puppeteer/index.ts
@@ -1,19 +1,19 @@
 "use server";
-const chromium = require("@sparticuz/chromium-min");
-const puppeteer = require("puppeteer-core");
+const chromium = require('@sparticuz/chromium-min');
+const puppeteer = require('puppeteer-core');
 
 const getHTMLFromURL = async (urlToScrape: string) => {
   try {
     const browser = await puppeteer.launch({
-      args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
-      defaultViewport: chromium.defaultViewport,
-      executablePath: await chromium.executablePath(
-        `https://github.com/Sparticuz/chromium/releases/download/v119.0.0/chromium-v116.0.0-pack.tar`
-      ),
-      headless: chromium.headless,
-      ignoreHTTPSErrors: true,
-    });
-
+        args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
+        defaultViewport: chromium.defaultViewport,
+        executablePath: await chromium.executablePath(
+          `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
+        ),
+        headless: chromium.headless,
+        ignoreHTTPSErrors: true,
+      });
+      
     const page = await browser.newPage();
     await page.goto(urlToScrape);
     const elementContent = await page.evaluate(() => document.body.innerHTML);
