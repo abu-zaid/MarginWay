4d885eafd0af29443c278759197459013919a802
Abu Zaid <iam.abuzaid@gmail.com>
2024-01-27 13:32:25 +0530

    refactor: file name update

--- lib/puppeteer/index.ts
+++ lib/puppeteer/index.ts
@@ -3,15 +3,18 @@ import puppeteer, { Browser } from "puppeteer";
 
 const getHTMLFromURL = async (urlToScrape: string) => {
   try {
-    const browser: Browser = await puppeteer.launch({ headless: "new" });
+    const browser: Browser = await puppeteer.launch({
+      headless: "new",
+      args: ["--no-sandbox", "--disable-dev-shm-usage"],
+    });
     const page = await browser.newPage();
-    await page.goto(urlToScrape);
-    const elementContent = await page.evaluate(() => (document.body.innerHTML));
+    await page.goto(urlToScrape, { waitUntil: "domcontentloaded" });
+    const elementContent = await page.evaluate(() => document.body.innerHTML);
     browser.close();
     return elementContent;
   } catch (error) {
     console.error("An error occurred while scraping the website: ", error);
-    return '';
+    return "";
   }
 };
 
