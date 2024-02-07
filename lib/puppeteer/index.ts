f4d304c845ff850e282c9c57b669364858471720
Abu Zaid <iam.abuzaid@gmail.com>
2024-02-07 21:00:24 +0530

    build: use normal puppeteer

--- lib/puppeteer/index.ts
+++ lib/puppeteer/index.ts
@@ -1,15 +1,12 @@
 "use server";
 const chromium = require('@sparticuz/chromium-min');
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer');
 
 const getHTMLFromURL = async (urlToScrape: string) => {
   try {
     const browser = await puppeteer.launch({
         args: [...chromium.args, '--hide-scrollbars', '--disable-web-security'],
         defaultViewport: chromium.defaultViewport,
-        executablePath: await chromium.executablePath(
-          `https://github.com/Sparticuz/chromium/releases/download/v116.0.0/chromium-v116.0.0-pack.tar`
-        ),
         headless: chromium.headless,
         ignoreHTTPSErrors: true,
       });
