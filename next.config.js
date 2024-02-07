da4b4acf5495e7d8033207a2e45acc1042f1d36d
Abu Zaid <iam.abuzaid@gmail.com>
2024-02-07 22:04:42 +0530

    build: add packages

--- next.config.js
+++ next.config.js
@@ -3,7 +3,12 @@ const nextConfig = {
     experimental: {
       serverActions: true,
       serverMinification: false,
-      serverComponentsExternalPackages: ['mongoose']
+      serverComponentsExternalPackages: [
+        'mongoose',
+        "puppeteer-extra",
+        "puppeteer-extra-plugin-stealth",
+        "puppeteer-extra-plugin-recaptcha",
+      ],
     },
     images: {
       domains: ['m.media-amazon.com','rukminim2.flixcart.com']
